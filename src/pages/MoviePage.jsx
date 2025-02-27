450a9a1999cc84fe91141c1cfcc8f1171aa98885
erikaCutugno <erikucciapop@gmail.com>
2025-02-27 16:16:10 +0100

    add image movie page

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -20,30 +20,31 @@ export default function MoviePage() {
   useEffect(fetchMovie, [id]);
   return (
     <div className="p-6">
-      <Container>
+      <Container size="sm">
         <Link to="/">
           <i className="fa-solid fa-house my-6 text-rose-800"></i>
           <span className="px-3 text-rose-800 text-lg font-medium">Home</span>
         </Link>
-        <div className="flex flex-wrap justify-between gap-8">
-          <div className="bg-slate-800 text-white rounded-xl  shadow p-4">
+        <div className="flex flex-wrap bg-slate-800 rounded-xl shadow ">
+          <div className=" w-full sm:w-1/3">
+            <img src={movie.image} alt={movie.title} className="w-full" />
+          </div>
+          <div className="text-white p-4 space-y-6 sm:w-2/3">
             <Heading level={2}>{movie.title}</Heading>
-            <Heading className="px-6 pt-6 pb-4" level={6}>
-              {movie.director}
-            </Heading>
-            <p className="text-sm px-6 py-4">Genere: {movie.genre}</p>
-            <p className="text-sm px-6 py-4">{movie.abstract}</p>
+            <Heading level={6}>{movie.director}</Heading>
+            <p className="text-sm">Genere: {movie.genre}</p>
+            <p className="text-sm">{movie.abstract}</p>
           </div>
-          <div className="bg-slate-800 text-white rounded-xl  shadow p-4 ">
-            <Heading level={2}>Recensioni</Heading>
-            <ul className="p-6">
-              {movie.review.map((elm) => (
-                <li className="p-2" key={elm.id}>
-                  <Review name={elm.name} vote={elm.vote} text={elm.text} />
-                </li>
-              ))}
-            </ul>
-          </div>
+        </div>
+        <div className="bg-slate-800 text-white rounded-xl mt-6 shadow p-4 ">
+          <Heading level={2}>Recensioni</Heading>
+          <ul className="p-6">
+            {movie.review.map((elm) => (
+              <li className="p-2" key={elm.id}>
+                <Review name={elm.name} vote={elm.vote} text={elm.text} />
+              </li>
+            ))}
+          </ul>
         </div>
       </Container>
     </div>

b25e4de6f603239882da39c701bd5890b76dc09d
erikaCutugno <erikucciapop@gmail.com>
2025-03-03 11:48:29 +0100

    add form create new movie

--- src/components/ui/Container.jsx
+++ src/components/ui/Container.jsx
@@ -1,7 +1,9 @@
-export default function Container({ children, size = "md" }) {
+export default function Container({ children, size = "md", className }) {
   const sizes = {
     sm: "max-w-5xl",
     md: "max-w-7xl",
   };
-  return <div className={`mx-auto px-4 ${sizes[size]}`}>{children}</div>;
+  return (
+    <div className={`mx-auto px-4 ${sizes[size]} ${className}`}>{children}</div>
+  );
 }
